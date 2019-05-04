6e6fdd5f7060ffe62559155d87205bbe70c08887
mahendras <mahendrasharma.sharma369@gmail.com>
2019-05-04 17:03:50 +0530

    first commit

---
+++ lib/CustomShapeClipper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomShapeClipper extends CustomClipper {
+
+  @override
+  getClip(Size size) {
+    // TODO: implement getClip
+    return null;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
+
+}
